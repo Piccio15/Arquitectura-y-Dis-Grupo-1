0c15e0b413f8ecf3354ec8b14b52f06b1645d22d
JuanseGenca <juanssegenca@gmail.com>
2026-05-08 14:54:33 -0300

    actualizo esquema de bd y agrego un post protegido

--- backend-estacionamiento/src/app.ts
+++ backend-estacionamiento/src/app.ts
@@ -1,15 +1,26 @@
 import express, { Request, Response } from 'express';
-import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
+import { clerkMiddleware } from '@clerk/express';
 
 // Inicializamos Express y Prisma
 const app = express();
 const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
-app.use(cors());
+// 1. Le decimos a TypeScript que el Request de Express ahora incluye "auth"
+declare global {
+  namespace Express {
+    interface Request {
+      auth?: {
+        userId: string | null;
+        sessionId?: string | null;
+      };
+    }
+  }
+}
+
 app.use(express.json());
+app.use(clerkMiddleware());
 
 // ==========================================
 // RUTAS DE PRUEBA: ZONAS
@@ -19,7 +30,6 @@ app.use(express.json());
 app.post('/api/zonas', async (req: Request, res: Response) => {
   try {
     const { nombre, calles, precio_hora } = req.body;
-    
     const nuevaZona = await prisma.zona.create({
       data: {
         nombre: nombre,
@@ -27,7 +37,6 @@ app.post('/api/zonas', async (req: Request, res: Response) => {
         precio_hora: precio_hora
       }
     });
-    
     res.status(201).json({ mensaje: "Zona creada con éxito", zona: nuevaZona });
   } catch (error) {
     console.error(error);
@@ -43,6 +52,26 @@ app.get('/api/zonas', async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error al obtener las zonas" });
+  }
+});
+
+// Ruta Protegida de prueba
+app.post('/api/estacionamiento/iniciar', async (req: Request, res: Response) => {
+  if (!req.auth?.userId) {
+     return res.status(401).json({ 
+       error: "Acceso denegado. Debes iniciar sesión para estacionar." 
+     });
+  }
+
+  const clerkUserId = req.auth.userId;
+
+  try {
+      res.json({ 
+        mensaje: "Estacionamiento iniciado con éxito", 
+        usuarioAuntenticado: clerkUserId 
+      });
+  } catch (error) {
+      res.status(500).json({ error: "Error en el servidor" });
   }
 });
 
