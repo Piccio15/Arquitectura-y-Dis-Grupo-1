797d766f7e3d5fbc37335fef530c4874c05a029c
JuanseGenca <juanssegenca@gmail.com>
2026-05-08 18:05:21 -0300

    borre funciones de prueba del backend

--- backend-estacionamiento/src/app.ts
+++ backend-estacionamiento/src/app.ts
@@ -29,60 +29,8 @@ app.use(cors({
 app.use(express.json());
 app.use(clerkMiddleware());
 
-// ==========================================
-// RUTAS DE PRUEBA: ZONAS
-// ==========================================
 
-// 1. POST: Crear una nueva zona
-app.post('/api/zonas', async (req: Request, res: Response) => {
-  try {
-    const { nombre, calles, precio_hora } = req.body;
-    const nuevaZona = await prisma.zona.create({
-      data: {
-        nombre: nombre,
-        calles: calles,
-        precio_hora: precio_hora
-      }
-    });
-    res.status(201).json({ mensaje: "Zona creada con éxito", zona: nuevaZona });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error interno al crear la zona" });
-  }
-});
-
-// 2. GET: Obtener todas las zonas
-app.get('/api/zonas', async (req: Request, res: Response) => {
-  try {
-    const zonas = await prisma.zona.findMany();
-    res.status(200).json(zonas);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error al obtener las zonas" });
-  }
-});
-
-// Ruta Protegida de prueba
-app.post('/api/estacionamiento/iniciar', async (req: Request, res: Response) => {
-  if (!req.auth?.userId) {
-     return res.status(401).json({ 
-       error: "Acceso denegado. Debes iniciar sesión para estacionar." 
-     });
-  }
-
-  const clerkUserId = req.auth.userId;
-
-  try {
-      res.json({ 
-        mensaje: "Estacionamiento iniciado con éxito", 
-        usuarioAuntenticado: clerkUserId 
-      });
-  } catch (error) {
-      res.status(500).json({ error: "Error en el servidor" });
-  }
-});
-
-// En tu archivo de rutas de usuario INTENTO CREAR UN USUARIO SI ES LA PRIMERA VEZ QUE INICIA SESIÓN
+//INTENTO CREAR UN USUARIO SI ES LA PRIMERA VEZ QUE INICIA SESIÓN ESTE POST NO BORRAR, ESTA FUNCIONANDO. faltaria realocar.
 app.post('/api/usuarios/sync', async (req: Request, res: Response) => {
   const { clerk_id, email } = req.body; // Estos vienen del frontend tras el login
 
